68ba195b508a0bcdc93d4c1ea87cacc97dfff019
alisonands <131694547+alisonands@users.noreply.github.com>
2024-04-20 23:58:37 -0400

    updates
    lotsa js css and html. updates on 3scroll and base websites.
    
    To do: make 2nd interactive html webpage

--- 3scroll.js
+++ 3scroll.js
@@ -1,3 +1,5 @@
+// ------FADE------
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry)=>{
         console.log(entry)
@@ -12,3 +14,15 @@ const observer = new IntersectionObserver((entries) => {
 // select all elements hidden
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+// CUSTOM CURSOR
+const cursorSmall = document.querySelector('.small');
+
+const positionElement = (e)=> {
+  const mouseY = e.clientY + window.scrollY;
+  const mouseX = e.clientX + window.scrollX;
+
+  cursorSmall.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+}
+
+window.addEventListener('mousemove', positionElement)
