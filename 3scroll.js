3bd7a71de6e8ddd58efe9733c69fe403ef6d753e
alisonands <131694547+alisonands@users.noreply.github.com>
2024-04-21 08:47:16 -0400

    updates

--- 3scroll.js
+++ 3scroll.js
@@ -15,14 +15,3 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
-// CUSTOM CURSOR
-const cursorSmall = document.querySelector('.small');
-
-const positionElement = (e)=> {
-  const mouseY = e.clientY + window.scrollY;
-  const mouseX = e.clientX + window.scrollX;
-
-  cursorSmall.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-}
-
-window.addEventListener('mousemove', positionElement)
